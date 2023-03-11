972fd4f3b40d0ff81bf638f68537abb02e70ee08
Nathan MacDiarmid <nathanmacdiarmid@gmail.com>
2023-03-11 12:38:56 -0500

    Fixed merge conflict

--- tests/FloorTest.java
+++ tests/FloorTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import src.Elevator;
-import src.Floor;
-import src.InputData;
-import src.Scheduler;
+import Elevator;
+import Floor;
+import InputData;
+import Scheduler;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
